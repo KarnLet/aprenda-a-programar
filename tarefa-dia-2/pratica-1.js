065cb0a2777e5ea5f06b2109ad62efd769573e5c
Karen <karnleticia@gmail.com>
2022-08-24 19:41:31 -0300

    changes

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,3 +2,7 @@ let Nome = "Karen"
 let Email = "karnleticia@gmail.com"
 let Profissão = "Professora"
 let Idade = "21"
+console.log(Nome)
+console.log(Email)
+console.log(Profissão)
+console.log(Idade)
